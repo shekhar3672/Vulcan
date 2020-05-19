57da90b0adf34408e1ff26e6ccae22c0cd835962
shekhar3672 <jadhavshekhar444@gmail.com>
2020-05-19 17:43:23 +0530

    Modified some text and package.json file.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Link to="/about/">About</Link>
       <Link to="/contact/">Contacts</Link>
       <Header headerText="This is index.js Header" />
-      <p>What a world.</p>
+      <p>Welcome to Vulcan.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
