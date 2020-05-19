98a34242f2b880bda9176909f58f43504e6a4266
shekhar3672 <jadhavshekhar444@gmail.com>
2020-05-19 17:17:52 +0530

    Initial commit for vulcan deployment demo

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/about/">About</Link>
+      <Link to="/contact/">Contacts</Link>
+      <Header headerText="This is index.js Header" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
